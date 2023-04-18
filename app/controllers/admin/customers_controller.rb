693552e8352d52dbbe89e62eef618d2b99e5744e
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-04-18 13:45:05 +0000

    [ADD][admin]ログイン/会員一覧/会員詳細/会員情報編集+[FIX]ヘッダー

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,26 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers=Customer.all.page(params[:page]).per(10)
   end
 
   def show
+    @customer=Customer.find(params[:id])
   end
 
   def edit
+    @customer=Customer.find(params[:id])
   end
+  
+  def update
+    @customer.update(customer_params)
+    redirect_to edit_admin_customer_path(customer)
+  end
+  
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :address, :telephone_number, :email, :membership_status)
+  end
+
 end
