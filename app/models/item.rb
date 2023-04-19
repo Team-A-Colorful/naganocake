3a07be4c5adc780b6f59fc719368e22aa843e41b
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-04-19 02:59:42 +0000

    [Add] genre admin_item　function

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,14 @@ class Item < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   attachment :image
+  
+  
+  #税込価格を計算するためのメソットです。
+  #viewページで下記のように呼び出してもらえれば表示できます。
+  #<%= @item.add_tax_salse_price.to_s(:delimited) %>
+  #(:delimited)は数値を3桁区切りにするためのものです。例: 12345678 → 12,345,678
+  def add_tax_sales_price
+    (self.price * 1.10).round
+  end
+  
 end
