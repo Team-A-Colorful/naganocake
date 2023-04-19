de7a204cf1e8e4169669226b025c46bb7295ed44
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-04-19 12:49:57 +0000

    [ADD]配送先一覧・追加画面、配送先編集画面

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,7 +1,30 @@
 class Public::ShippingAddressesController < ApplicationController
   def index
+    @customer = current_customer
+    @shipping_address = ShippingAddress.new
+    @shipping_addresses = @customer.shipping_addresses.all
   end
 
   def edit
+    @shipping_address = ShippingAddress.find(params[:id])
   end
+  
+  def create
+    shipping_address = ShippingAddress.new(shipping_address_params)
+    shipping_address.customer=current_customer
+    shipping_address.save
+    redirect_to request.referer
+  end
+  
+  def update
+    @shipping_address = ShippingAddress.find(params[:id])
+    @shipping_address.update(shipping_address_params)
+    redirect_to shipping_addresses_path
+  end
+  
+  
+   def shipping_address_params
+    params.require(:shipping_address).permit(:address_label, :post_code, :address)
+  end
+  
 end
