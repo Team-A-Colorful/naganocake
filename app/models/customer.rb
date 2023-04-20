f3c382c6273415421cc978f4d8aa9bdb48674f7d
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-04-20 15:37:46 +0000

    電話番号バリデーションを正規表現化

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,9 +6,8 @@ class Customer < ApplicationRecord
   validates :last_name,presence:true
   validates :first_name_kana,presence:true
   validates :last_name_kana,presence:true
-  validates :post_code,presence:true
   validates :address,presence:true
-  validates :telephone_number,presence:true
+  validates :telephone_number,format: {with: /\A0\d{9,10}\z/}
   validates :membership_status,presence:true
   validates :email,presence:true
 
