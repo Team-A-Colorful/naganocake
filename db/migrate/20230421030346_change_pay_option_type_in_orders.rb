2ee857285eb93895fe706f91ce0bea28085e3851
daze573 <124431662+daze573@users.noreply.github.com>
2023-04-21 04:35:57 +0000

    [Add] 管理者側詳細画面

--- db/migrate/20230421030346_change_pay_option_type_in_orders.rb
+++ db/migrate/20230421030346_change_pay_option_type_in_orders.rb
@@ -1,5 +1,5 @@
 class ChangePayOptionTypeInOrders < ActiveRecord::Migration[6.1]
   def change
-    # change_column :orders, :pay_option, :integer
+    change_column :orders, :pay_option, :integer
   end
 end
