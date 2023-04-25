a77c80bc723ca42ec2a42a3488fc42b49c10faf1
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-04-25 03:32:28 +0000

    [Fix] 商品編集画面パス　値段表記統一

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -31,7 +31,7 @@ class Admin::ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to admin_items_path
+      redirect_to admin_item_path(@item)
     else
       render :edit
     end
